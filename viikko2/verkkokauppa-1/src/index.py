8d2bd6275508c5d39c729b3ab7ac02ce77e24f29
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-11-14 21:03:13 +0200

    week 2 done

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,18 +1,12 @@
 from kauppa import Kauppa
-from kirjanpito import Kirjanpito
-from varasto import Varasto
-from pankki import Pankki
-from viitegeneraattori import Viitegeneraattori
+from kirjanpito import the_kirjanpito_olio
 
 
 def main():
-    kauppa = Kauppa(
-        Varasto.get_instance(),
-        Pankki.get_instance(),
-        Viitegeneraattori.get_instance()
-    )
+    kauppa = Kauppa()
 
-    # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+
+# kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
     kauppa.lisaa_koriin(1)
     kauppa.lisaa_koriin(3)
@@ -20,7 +14,7 @@ def main():
     kauppa.poista_korista(1)
     kauppa.tilimaksu("Pekka Mikkola", "1234-12345")
 
-    # seuraava asiakas
+  # seuraava asiakas
     kauppa.aloita_asiointi()
 
     for _ in range(0, 24):
@@ -29,7 +23,7 @@ def main():
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
 
     # kirjanpito
-    for tapahtuma in Kirjanpito.get_instance().tapahtumat:
+    for tapahtuma in the_kirjanpito_olio.tapahtumat:
         print(tapahtuma)
 
 
