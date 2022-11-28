638212ad62306bf180a6f474779bf9c37db9182a
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-11-28 15:55:14 +0200

    5.3 ostoskorin hinta sama kuin yhden lisätyn tuotteen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -14,7 +14,7 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
 
     def hinta(self):
-        return 0
+        return sum(tuote.hinta() for tuote in self.ostokset)
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
