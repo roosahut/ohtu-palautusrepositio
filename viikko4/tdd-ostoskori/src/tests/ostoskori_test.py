b795b363c00fc30d634a1c0b6d7e5db03e73e6b2
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-11-28 16:06:20 +0200

    5.6 ja korjaa edelliset eri tuotteiden testit

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,33 +6,37 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote('Maito', 3)
+        self.leipa = Tuote('Leipä', 5)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote('Maito', 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_hinta_tuotteen_hinta(self):
-        maito = Tuote('Maito', 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.hinta(), 3)
 
-    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito = Tuote('Maito', 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(maito)
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.leipa)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
-    def test_kahden_tuotteen_lisaamisen_jalkeen_hinta_yhteishinta(self):
-        maito = Tuote('Maito', 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(maito)
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_hinta_yhteishinta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.leipa)
 
-        self.assertEqual(self.kori.hinta(), 6)
+        self.assertEqual(self.kori.hinta(), 8)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
